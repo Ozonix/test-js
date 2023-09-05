2d20faf3dee6a0b59cde97a668b99daa7e35a9f4
Руслан Белов <v2@Ruslans-MacBook-Air.local>
2023-09-05 12:08:48 +0300

    test

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const exec = require('executive');
+const bodyParser = require("body-parser");
+const async = require("async");
+const path = require("path");
+const http = require('http');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(`${__dirname}`))
+
+app.post('/proxy/', function (client_req, client_res) {
+
+  
+
+  var client = client_req.body;
+
+  const postData = querystring.stringify(client.data);
+  const headers = client.headers || {}
+
+  var options = {
+    hostname: client.host,
+    port: 80,
+    path: client.path,
+    method: client.method,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData),
+      ...headers
+    }
+  };
+
+  let data = ``
+
+  const req = http.request(options, function (res) {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      data += `${chunk}`
+    });
+
+    res.on('end', () => {
+      if (IsJsonString(data)) {
+        client_res.send({
+          data: JSON.parse(data)
+        })
+      } else {
+        client_res.send({
+          data: {
+            paths: {
+              "/to/on/khujy/": {
+                put: {
+                  description: "Эндпоинт успешного получения данных.",
+                  responses: {
+                    "200": {
+                      "data": "ХУЙ!"
+                    }
+                  }
+                },
+                parameters: []
+              }
+            }
+          }
+        })
+      }
+      
+    })
+  });
+
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  req.write(postData);
+
+  client_req.pipe(req, {
+    end: true
+  });
+});
+
+app.listen(10101, ()=> {
+  console.log('Доступно на http://localhost:10101')
+});
